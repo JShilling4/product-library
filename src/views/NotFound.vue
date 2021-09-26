8cd263bb5d1b5e300dcd6b381820cd7ae6dcbde8
Justin Shilling <loveofmusic25@hotmail.com>
2021-09-26 01:03:55 -0400

    not found page scaffolding

---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="notFound container">
+        <app-button
+            class="backBtn"
+            background-color="var(--primary-color)"
+            color="#fff"
+            border="none"
+            @click="$router.push('/')"
+        >Home</app-button>
+        <h1>404 - Page Not Found</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound",
+    }
+</script>
+
+<style lang="scss" scoped>
+.notFound {
+    .backBtn {
+        margin-top: 3rem;
+    }
+
+    h1 {
+        margin-top: 4rem;
+        text-align: center;
+    }
+}
+</style>
