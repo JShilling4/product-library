ef30397447c9a0a2c5721da5a552c6541e1aab33
Justin Shilling <loveofmusic25@hotmail.com>
2021-09-26 20:57:47 -0400

    button adjustments

--- src/components/global/AppButton.vue
+++ src/components/global/AppButton.vue
@@ -1,47 +1,56 @@
 <template>
-    <button class="button" :style="styleObject">
+    <button
+        class="button"
+        :style="styleObject"
+    >
         <slot></slot>
     </button>
 </template>
 
 <script>
-    export default {
-        name: "AppButton",
-        props: {
-            color: {
-                type: String,
-            },
-            backgroundColor: {
-                type: String,
-            },
-            width: {
-                type: String,
-            },
-            fontWeight: {
-                type: String,
-            },
-            border: {
-                type: String,
-            },
+export default {
+    name: "AppButton",
+    props: {
+        color: {
+            type: String,
         },
-        data() {
-            return {
-                styleObject: {
-                    color: this.color || "#333",
-                    backgroundColor: this.backgroundColor || "#fff",
-                    height: this.height || "4rem",
-                    width: this.width || "12rem",
-                    fontWeight: this.fontWeight || "600",
-                    border: this.border || "1px solid #333",
-                    borderRadius: this.borderRadius || "5px",
-                }
-            };
-        }
-    }
+        backgroundColor: {
+            type: String,
+        },
+        width: {
+            type: String,
+        },
+        fontSize: {
+            type: String,
+        },
+        fontWeight: {
+            type: String,
+        },
+        border: {
+            type: String,
+        },
+        borderRadius: {
+            type: String,
+        },
+    },
+    data() {
+        return {
+            styleObject: {
+                color: this.color || "#333",
+                backgroundColor: this.backgroundColor || "#fff",
+                height: this.height || "4rem",
+                width: this.width || "12rem",
+                border: this.border || "1px solid #333",
+                borderRadius: this.borderRadius || "5px",
+            },
+        };
+    },
+};
 </script>
 
 <style lang="scss" scoped>
 .button {
+    position: relative;
     outline: none;
     border: none;
     letter-spacing: 1.2px;
